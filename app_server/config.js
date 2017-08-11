ae69a7359134165274d8c857e758d8f1a0704fc1
Scott Westover <scottwestover2006@gmail.com>
2017-08-10 20:31:28 -0400

    [Add] Added api keys section to config.

--- app_server/config.js
+++ app_server/config.js
@@ -2,6 +2,12 @@
 /*jshint esversion: 6 */
 'use strict';
 
+require('dotenv').config();
+
 module.exports = {
-  "port": 8081
+  "port": 8081,
+  "consumer_key": process.env.APP_KEY,
+  "consumer_secret": process.env.APP_SECRET,
+  "token": process.env.ACCESS_TOKEN,
+  "token_secret": process.env.ACCESS_TOKEN_SECRET
 };
