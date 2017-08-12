8a7fd0396250a555d96ae1f8e66ca5a25adf86ee
Scott Westover <scottwestover2006@gmail.com>
2017-08-11 21:55:40 -0400

    [Add] Added more data points for developing dashboard.

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import dataInt from '../data/dataInt';
 import axios from 'axios';
 import config from '../config.js';
+
+import dataInt from '../data/dataInt';
+import qh from '../data/queueHealth';
+import engagementActivity from '../data/engagementActivity';
+import agentActivity from '../data/agentActivity';
 
 Vue.use(Vuex);
 
@@ -28,7 +32,7 @@ const actions = {
   initData: ({commit}) => {
     commit('SET_DASH',dataInt);
   },
-  fetchQueueHealth: ({ commit }) => {
+  fetchQueueHealth2: ({ commit }) => {
     axios.get(config.apiURL + '/api/v1/getQueueHealth')
       .then(response => {
         commit("FETCH_QUEUE_HEALTH", response.data);
@@ -36,7 +40,10 @@ const actions = {
       .catch(error => {
         console.log(error.statusText);
       });
-  }
+  },
+  fetchQueueHealth: ({ commit }) => {
+    commit('FETCH_QUEUE_HEALTH',qh);
+  },
 };
 
 const getters = {
