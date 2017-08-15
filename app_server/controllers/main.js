67330683d471a005354fc5c02ed0dce4dec74f49
Scott Westover <scottwestover2006@gmail.com>
2017-08-15 19:21:33 -0400

    [Add] Added bearer token method.

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -4,14 +4,14 @@
 
 const request = require('request');
 const config = require('../config');
-const Domain = require("le-api").Domain;
-
-let domain = new Domain();
+const Domain = require('le-api').Domain; 
+const Credentials = require('le-api').Credentials;
 
 const main = {
   test: function (req, res) {
     res.json({'hi':1});
   },
+  
   getQueueHealth: function(req, res) {
     let oauth = {
       consumer_key: config.consumer_key,
@@ -31,26 +31,56 @@ const main = {
       res.json(b);
     });
   },
+
   getDomain: (req, res) => {
+    let domain = new Domain();
     let accountNumber = req.body.accountNumber;
     domain.getLeDataReportingDomain(accountNumber, function(err, data) {
-      if(err){
-        res.status(401);
-        res.json({
-          "status": 401,
-          "message": "Error",
-          "error": err
-        });
+      if (err) {
+        sendErrorResponse(err, res);
       }else{
-        res.status(200);
-        res.json({
-          "status": 200,
-          "message": "Success",
-          "data": data
+        sendSuccessResponse(data, res);
+      }
+    });
+  },
+
+  getBearerToken: (req, res) => {
+    let creds = new Credentials();
+    let accountNumber = req.body.accountNumber;
+    let userName = req.body.userName;
+    let password = req.body.password;
+    creds.init(accountNumber, null, { username: userName, password: password }, (err, data) => {
+      if (err) {
+        sendErrorResponse(err, res);
+      }else{
+        creds.login(function(err, data) {
+          if(err){
+            sendErrorResponse(err, res);
+          }else{
+            sendSuccessResponse(creds.bearerToken, res);
+          }
         });
       }
     });
   }
 };
+
+let sendErrorResponse = (error, response) => {
+  response.status(401);
+  response.json({
+    "status": 401,
+    "message": "Error",
+    "error": error
+  });
+};
+
+let sendSuccessResponse = (data, response) => {
+  response.status(200);
+  response.json({
+    "status": 200,
+    "message": "Success",
+    "data": data
+  });
+}
 
 module.exports = main;
