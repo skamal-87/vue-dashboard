829a5bdb479903a8a2612485681d36cb64fc367c
Steve Kamal <skamal@liveperson.com>
2017-08-15 16:47:44 -0400

    created settings component that uses common api and le auth components. settings component displays the authentication that was used at login. WIP fixing a lot more

---
+++ src/components/settings/Settings.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+        <p>The Settings Component</p>
+        <div v-show="usingKeys"> 
+            <app-keys-display></app-keys-display>
+        </div>
+        <div v-show="!usingKeys"> 
+            <app-login-display></app-login-display>
+        </div> 
+</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import KeysDisplay from './KeysDisplay.vue'
+import LoginDisplay from './LoginDisplay.vue'
+  export default {
+    computed: {
+        ...mapGetters({
+            usingKeys: 'usingKeys'
+        })
+    },
+    data() {
+      return {
+
+      }
+    },
+    components: {
+        appKeysDisplay: KeysDisplay,
+        appLoginDisplay: LoginDisplay
+    },
+    methods: {
+      submit() {
+        var credentials = {
+
+        }
+      }
+    }
+
+  }
+  </script>
+  <style scoped>
+  .nav-bar {
+    position: absolute;
+    width: 90px;
+    height: 100%;
+    padding-left: 5px;
+    text-align: center;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0px;
+    padding-top: 5px;
+  }
+
+  li {
+    margin-bottom: 10px;
+  }
+
+  a {
+    color: #00d1b2;
+    text-decoration: none;
+  }
+
+  a:hover, a:hover > i {
+    color: white;
+  }
+
+  i {
+    width: 100%;
+    color: #00d1b2;
+    font-size: 25px;
+    padding-top: 10px;
+  }
+
+  .hidden-nav {
+    display: none;
+  }
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import EngagementActivity from './components/chat/other/EngagementActivity.vue';
 import SLA from './components/chat/other/SLA.vue';
 import CurrentQueue from './components/chat/other/CurrentQueue.vue';
 import Login from './components/Login.vue'
+import Settings from './components/settings/Settings.vue'
 
 export const routes = [
   { path: '', component: ChatDashboard },
@@ -13,6 +14,7 @@ export const routes = [
   { path: '/engagementActivityDashboard', component: EngagementActivity },
   { path: '/currentQueue', component: CurrentQueue },
   { path: '/slaDashboard', component: SLA },
-  { path: '/login', component: Login}
+  { path: '/login', component: Login},
+  { path: '/settings', component: Settings}
   
 ];
