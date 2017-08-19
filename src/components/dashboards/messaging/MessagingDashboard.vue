f6052ed4c3471cdd94e2801bb4f922a3b6b127d9
Scott Westover <scottwestover2006@gmail.com>
2017-08-19 17:37:47 -0400

    [Add] More work on graphs.

--- src/components/dashboards/messaging/MessagingDashboard.vue
+++ src/components/dashboards/messaging/MessagingDashboard.vue
@@ -4,10 +4,10 @@
     <TopBar class="tob-bar-metrics" :topBar="messagingAgentMetrics" :bottomBar="messagingCsatMetrics"></TopBar>
     <div class="row tob-bar-metrics">
       <div class="col-sm-6">
-        <canvas id="myChart23" style="width: 100%;height: 400px;"></canvas>
+        <canvas id="conversationsChart" style="width: 100%;height: 400px;"></canvas>
       </div>
       <div class="col-sm-6">
-        <canvas id="myChart24" style="width: 100%;height: 400px;"></canvas>
+        <canvas id="csatChart" style="width: 100%;height: 400px;"></canvas>
       </div>
     </div>
   </section>
@@ -22,36 +22,39 @@
     components: {
       TopBar
     },
+    data: () => {
+      return {
+        conversationsChart: null,
+        csatChart: null,
+        conversationsChartData: [12, 19, 3],
+        csatChartData: [12, 19, 3, 5, 2]
+      }
+    },
     created() {
       this.$store.dispatch('fetchMessageSummary');
       this.$store.dispatch('fetchMessageConversation');
       this.$store.dispatch('fetchMessageCsatDistribution');
     },
     mounted() {
-      var ctx = document.getElementById("myChart23");
-      var ctx2 = document.getElementById("myChart24");
-      var myChart = new Chart(ctx, {
+      let ctxConversations = document.getElementById("conversationsChart");
+      let ctxCSAT = document.getElementById("csatChart");
+      this.conversationsChart = new Chart(ctxConversations, {
         type: 'bar',
         data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          labels: ["CCP", "Consumer", "System"],
           datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: '# of Conversations Resolved',
+            data: this.conversationsChartData,
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
+              'rgba(255, 206, 86, 0.2)'
             ],
             borderColor: [
               'rgba(255,99,132,1)',
               'rgba(54, 162, 235, 1)',
               'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
+              'rgba(75, 192, 192, 1)'
             ],
             borderWidth: 1
           }]
@@ -68,28 +71,26 @@
           responsive: false
         }
       });
-      var myChart2 = new Chart(ctx2, {
+      this.csatChart = new Chart(ctxCSAT, {
         type: 'bar',
         data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          labels: ["Answer 5", "Answer 4", "Answer 3", "Answer 2", "Answer 1"],
           datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: '# of Answers',
+            data: this.csatChartData,
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
               'rgba(54, 162, 235, 0.2)',
               'rgba(255, 206, 86, 0.2)',
               'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
+              'rgba(153, 102, 255, 0.2)'
             ],
             borderColor: [
               'rgba(255,99,132,1)',
               'rgba(54, 162, 235, 1)',
               'rgba(255, 206, 86, 1)',
               'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
+              'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 1
           }]
@@ -198,6 +199,14 @@
           }
         };
         return dataBar;
+      }
+    },
+    methods: {
+      updateChartData: (chart, data) => {
+        chart.data.datasets.forEach((dataset) => {
+          dataset.data = data;
+        });
+        chart.update();
       }
     }
   }
